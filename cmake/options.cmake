e85adfddec2e325728fde0b25f0267d48272f4db
lifeng68 <lifeng68@huawei.com>
2020-08-10 10:36:01 +0800

    Release 2.0.4
    Signed-off-by: lifeng68 <lifeng68@huawei.com>

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -33,7 +33,7 @@ endif()
 
 option(VERSION "set isulad version" ON)
 if (VERSION STREQUAL "ON")
-    set(ISULAD_VERSION "2.0.3")
+    set(ISULAD_VERSION "2.0.4")
 endif()
 
 option(DEBUG "set isulad gcc option" ON)
