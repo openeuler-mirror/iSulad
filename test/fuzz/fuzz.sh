d6271242db05616e98ed23ea9bbf8b23bdebb6d4
jikui <jikui2@huawei.com>
2020-08-26 11:08:13 +0800

    iSulad: add copyright for fuzz.sh
    Signed-off-by: jikui <jikui2@huawei.com>

--- test/fuzz/fuzz.sh
+++ test/fuzz/fuzz.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#######################################################################
+##- @Copyright (C) Huawei Technologies., Ltd. 2020. All rights reserved.
+# - iSulad licensed under the Mulan PSL v2.
+# - You can use this software according to the terms and conditions of the Mulan PSL v2.
+# - You may obtain a copy of Mulan PSL v2 at:
+# -     http://license.coscl.org.cn/MulanPSL2
+# - THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# - IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# - PURPOSE.
+# - See the Mulan PSL v2 for more details.
+##- @Description: fuzz test executive script
+##- @Author: jikui
+##- @Create: 2020-07-09
+#######################################################################
 
 current_dir=$(cd $(dirname $0) && pwd)
 FUZZ_OPTION="${current_dir}/corpus -dict=${current_dir}/dict/im_oci_image_exist_fuzz.dict -runs=1000000 -max_total_time=3600"
