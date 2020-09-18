fcad56c81f1bbe6dd594e6b9c43a563129f94ed7
lifeng68 <lifeng68@huawei.com>
2020-09-18 16:11:34 +0800

    CI: update registry from 163 to ali
    Signed-off-by: lifeng68 <lifeng68@huawei.com>

--- CI/make-and-install.sh
+++ CI/make-and-install.sh
@@ -89,7 +89,7 @@ cmake -DLIB_INSTALL_DIR=${restbuilddir}/lib -DCMAKE_INSTALL_PREFIX=${restbuilddi
 make -j $(nproc)
 make install
 sed -i 's/"log-driver": "stdout"/"log-driver": "file"/g' ${restbuilddir}/etc/isulad/daemon.json
-sed -i "/registry-mirrors/a\        \"https://hub-mirror.c.163.com\"" ${restbuilddir}/etc/isulad/daemon.json
+sed -i "/registry-mirrors/a\        \"https://3laho3y3.mirror.aliyuncs.com\"" ${restbuilddir}/etc/isulad/daemon.json
 
 #build grpc version
 cd $ISULAD_COPY_PATH
@@ -104,4 +104,4 @@ fi
 make -j $(nproc)
 make install
 sed -i 's/"log-driver": "stdout"/"log-driver": "file"/g' ${builddir}/etc/isulad/daemon.json
-sed -i "/registry-mirrors/a\        \"https://hub-mirror.c.163.com\"" ${builddir}/etc/isulad/daemon.json
+sed -i "/registry-mirrors/a\        \"https://3laho3y3.mirror.aliyuncs.com\"" ${builddir}/etc/isulad/daemon.json
