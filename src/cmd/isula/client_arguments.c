556b04ef7672263f9310baf5f6c09e022985869a
zhangxiaoyu <zhangxiaoyu58@huawei.com>
2020-10-13 21:43:29 +0800

    network: add network create client command
    Signed-off-by: zhangxiaoyu <zhangxiaoyu58@huawei.com>

--- src/cmd/isula/client_arguments.c
+++ src/cmd/isula/client_arguments.c
@@ -267,6 +267,18 @@ void client_arguments_free(struct client_arguments *args)
 
     free(custom_conf->stop_signal);
     custom_conf->stop_signal = NULL;
+
+    free(custom_conf->driver);
+    custom_conf->driver = NULL;
+
+    free(custom_conf->gateway);
+    custom_conf->gateway = NULL;
+
+    free(custom_conf->subnet);
+    custom_conf->subnet = NULL;
+
+    free(args->network_name);
+    args->network_name = NULL;
 }
 
 /* print common help */
---
+++ src/cmd/isula/network/network_create.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020. All rights reserved.
+ * iSulad licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: zhangxiaoyu
+ * Create: 2020-09-02
+ * Description: provide network create definition
+ ******************************************************************************/
+#ifndef CMD_ISULA_NETWORK_CREATE_H
+#define CMD_ISULA_NETWORK_CREATE_H
+
+#include "client_arguments.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NETWORK_CREATE_OPTIONS(cmdargs)                         \
+    {                                                           \
+        CMD_OPT_TYPE_STRING_DUP,                                \
+        false,                                                  \
+        "driver",                                               \
+        'd',                                                    \
+        &(cmdargs).custom_conf.driver,                          \
+        "Driver to manager the network (default \"bridge\")",   \
+        NULL                                                    \
+    },                                                          \
+    {                                                           \
+                                                                CMD_OPT_TYPE_STRING_DUP,                                \
+                                                                false,                                                  \
+                                                                "gateway",                                              \
+                                                                0,                                                      \
+                                                                &(cmdargs).custom_conf.gateway,                         \
+                                                                "IPv4 or IPv6 gateway for the subnet",                  \
+                                                                NULL                                                    \
+    },                                                          \
+    {                                                           \
+                                                                CMD_OPT_TYPE_BOOL,                                      \
+                                                                false,                                                  \
+                                                                "internal",                                             \
+                                                                0,                                                      \
+                                                                &(cmdargs).custom_conf.internal,                        \
+                                                                "Restrict external access from this network",           \
+                                                                NULL                                                    \
+    },                                                          \
+    {                                                           \
+                                                                CMD_OPT_TYPE_STRING_DUP,                                \
+                                                                false,                                                  \
+                                                                "subnet",                                               \
+                                                                0,                                                      \
+                                                                &(cmdargs).custom_conf.subnet,                          \
+                                                                "Subnet in CIDR format",                                \
+                                                                NULL                                                    \
+    },
+
+extern const char g_cmd_network_create_desc[];
+extern const char g_cmd_network_create_usage[];
+extern struct client_arguments g_cmd_network_create_args;
+int cmd_network_create_main(int argc, const char **argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CMD_ISULA_NETWORK_CREATE_H
