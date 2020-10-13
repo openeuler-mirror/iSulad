199d45334d759a62a347dd16fce6ec89f7457f01
zhangxiaoyu <zhangxiaoyu58@huawei.com>
2020-10-13 22:07:26 +0800

    network: add network create CI testcase
    Signed-off-by: zhangxiaoyu <zhangxiaoyu58@huawei.com>

---
+++ CI/test_cases/network_cases/network_create.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+#
+# attributes: isulad basic network create
+# concurrent: NA
+# spend time: 15
+
+#######################################################################
+##- @Copyright (C) Huawei Technologies., Ltd. 2020. All rights reserved.
+# - iSulad licensed under the Mulan PSL v2.
+# - You can use this software according to the terms and conditions of the Mulan PSL v2.
+# - You may obtain a copy of Mulan PSL v2 at:
+# -     http://license.coscl.org.cn/MulanPSL2
+# - THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# - IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# - PURPOSE.
+# - See the Mulan PSL v2 for more details.
+##- @Description:CI
+##- @Author: zhangxiaoyu
+##- @Create: 2020-09-27
+#######################################################################
+
+curr_path=$(dirname $(readlink -f "$0"))
+source ../helpers.sh
+
+function test_network_create()
+{
+    local ret=0
+    local name1="cni1"
+    local name2="cni2"
+    local invalid_name=".xx"
+    local test="network create test => (${FUNCNAME[@]})"
+
+    msg_info "${test} starting..."
+
+    check_valgrind_log
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - stop isulad failed" && ((ret++))
+
+    start_isulad_with_valgrind
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - start isulad failed" && ((ret++))
+
+    isula network create ${name1} ${name2} 2>&1 | grep "at most 1 network name"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create networks success, but should failed" && return ${FAILURE}
+
+    isula network create ${invalid_name} 2>&1 | grep "Invalid network name"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network ${invalid_name} success, but should failed" && return ${FAILURE}
+
+    isula network create -d macvlan 2>&1 | grep "Cannot support driver"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network success with unsupported driver macvlan, but should failed" && return ${FAILURE}
+
+    isula network create --gateway 192.172.58.1 2>&1 | grep "Cannot specify gateway without subnet"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network success by specifing gateway without subnet, but should failed" && return ${FAILURE}
+
+    isula network create --subnet 192.172.58.0/33 2>&1 | grep "Invalid subnet"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network success by specifing invalid subnet, but should failed" && return ${FAILURE}
+
+    isula network create --subnet 192.172.58.0/24 --gateway 192.0.1 2>&1 | grep "Invalid gateway"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network success by specifing invalid gateway, but should failed" && return ${FAILURE}
+
+    isula network create --subnet 192.172.58.0/24 --gateway 192.0.0.1 2>&1 | grep "not match"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network success by specifing unmatch subnet gateway, but should failed" && return ${FAILURE}
+
+    file=$(isula network create --subnet 192.172.58.0/24 ${name1} | awk 'END {print}')
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network ${name1} failed" && return ${FAILURE}
+    [ ! -f ${file} ] && msg_err "${FUNCNAME[0]}:${LINENO} - file ${file} not exist" && return ${FAILURE}
+
+    isula network create ${name1} 2>&1 | grep "has been used"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network ${name1} success, but should failed" && return ${FAILURE}
+
+    isula network create --subnet 192.172.58.0/24 2>&1 | grep "conflict with CNI config or host network"
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network success by specifing conflict subnet, but should failed" && return ${FAILURE}
+
+    cat ${file} | grep '"subnet": "192.172.58.0/24"'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - no specified subnet in file" && return ${FAILURE}
+
+    cat ${file} | grep '"gateway": "192.172.58.1"'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - no gateway in file" && return ${FAILURE}
+
+    cat ${file} | grep '"name": "'${name1}'"'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - no specified name in file" && return ${FAILURE}
+
+    cat ${file} | grep '"isGateway": true'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - no isGateway in file" && return ${FAILURE}
+
+    cat ${file} | grep '"ipMasq": true'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - no ipMasq in file" && return ${FAILURE}
+
+    rm -f ${file}
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - rm ${file} failed" && return ${FAILURE}
+
+    file=$(isula network create -d bridge --internal | awk 'END {print}')
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network with specifing bridge failed" && return ${FAILURE}
+
+    cat ${file} | grep '"isGateway": true'
+    [[ $? -eq 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - find isGateway in file, but should not" && return ${FAILURE}
+
+    cat ${file} | grep '"ipMasq": true'
+    [[ $? -eq 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - find ipMasq in file, but should not" && return ${FAILURE}
+
+    rm -f ${file}
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - rm ${file} failed" && return ${FAILURE}
+
+    file=$(isula network create --subnet fff0:3::3/64 | awk 'END {print}')
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - create network with specifing IPv6 subnet" && return ${FAILURE}
+
+    cat ${file} | grep '"subnet": "fff0:3::3/64",'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - no specified subnet in file" && return ${FAILURE}
+
+    cat ${file} | grep '"gateway": "fff0:0003::0001"'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - no gateway in file" && return ${FAILURE}
+
+    rm -f ${file}
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - rm ${file} failed" && return ${FAILURE}
+
+    check_valgrind_log
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - stop isulad failed" && ((ret++))
+
+    start_isulad_with_valgrind
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - start isulad failed" && ((ret++))
+
+    msg_info "${test} finished with return ${ret}..."
+    return ${ret}
+}
+
+declare -i ans=0
+
+test_network_create || ((ans++))
+
+show_result ${ans} "${curr_path}/${0}"
