b9b2192ff44b19ae2d10082acab9b607c9167fa1
zhangxiaoyu <zhangxiaoyu58@huawei.com>
2020-11-27 15:37:52 +0800

    network: add network remove command
    Signed-off-by: zhangxiaoyu <zhangxiaoyu58@huawei.com>

--- src/daemon/entry/connect/rest/rest_network_service.c
+++ src/daemon/entry/connect/rest/rest_network_service.c
@@ -305,6 +305,108 @@ out:
     free_network_list_response(response);
 }
 
+/* network remove request check */
+static int network_remove_request_check(network_remove_request *req)
+{
+    if (req->name == NULL) {
+        ERROR("network name required!");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int network_remove_request_from_rest(evhtp_request_t *req, network_remove_request **request)
+{
+    int ret = 0;
+    size_t body_len = 0;
+    char *body = NULL;
+    parser_error err = NULL;
+
+    ret = get_body(req, &body_len, &body);
+    if (ret != 0) {
+        ERROR("Failed to get body");
+        return -1;
+    }
+
+    *request = network_remove_request_parse_data(body, NULL, &err);
+    if (*request == NULL) {
+        ERROR("Invalid request body:%s", err);
+        ret = -1;
+        goto out;
+    }
+
+    ret = network_remove_request_check(*request);
+
+out:
+    put_body(body);
+    free(err);
+    return ret;
+}
+
+/* evhtp send network remove repsponse */
+static void evhtp_send_network_remove_repsponse(evhtp_request_t *req, network_remove_response *response, int rescode)
+{
+    parser_error err = NULL;
+    struct parser_context ctx = { OPT_GEN_SIMPLIFY, 0 };
+    char *responsedata = NULL;
+
+    if (response == NULL) {
+        ERROR("Failed to generate network remove response info");
+        evhtp_send_reply(req, RESTFUL_RES_ERROR);
+        goto out;
+    }
+    responsedata = network_remove_response_generate_json(response, &ctx, &err);
+    if (responsedata == NULL) {
+        ERROR("Failed to generate network remove json:%s", err);
+        evhtp_send_reply(req, RESTFUL_RES_ERROR);
+        goto out;
+    }
+
+    evhtp_send_response(req, responsedata, rescode);
+
+out:
+    free(err);
+    free(responsedata);
+    return;
+}
+
+/* rest network remove cb */
+static void rest_network_remove_cb(evhtp_request_t *req, void *arg)
+{
+    int tret;
+    service_executor_t *cb = NULL;
+    network_remove_request *request = NULL;
+    network_remove_response *response = NULL;
+
+    // only deal with POST request
+    if (evhtp_request_get_method(req) != htp_method_POST) {
+        evhtp_send_reply(req, RESTFUL_RES_NOTIMPL);
+        return;
+    }
+    cb = get_service_executor();
+    if (cb == NULL || cb->network.remove == NULL) {
+        ERROR("Unimplemented callback");
+        evhtp_send_reply(req, RESTFUL_RES_NOTIMPL);
+        return;
+    }
+
+    tret = network_remove_request_from_rest(req, &request);
+    if (tret < 0) {
+        ERROR("Bad request");
+        evhtp_send_reply(req, RESTFUL_RES_SERVERR);
+        goto out;
+    }
+
+    (void)cb->network.remove(request, &response);
+
+    evhtp_send_network_remove_repsponse(req, response, RESTFUL_RES_OK);
+
+out:
+    free_network_remove_request(request);
+    free_network_remove_response(response);
+}
+
 /* rest register network handler */
 int rest_register_network_handler(evhtp_t *htp)
 {
@@ -320,6 +422,9 @@ int rest_register_network_handler(evhtp_t *htp)
         ERROR("Failed to register list callback");
         return -1;
     }
-
+    if (evhtp_set_cb(htp, NetworkServiceRemove, rest_network_remove_cb, NULL) == NULL) {
+        ERROR("Failed to register remove callback");
+        return -1;
+    }
     return 0;
 }
