3b6f11afadb42514088acca98506ef3fcb56b7c2
gaohuatao <gaohuatao@huawei.com>
2020-12-24 17:04:01 +0800

    add isula start -a CI test
    Signed-off-by: gaohuatao <gaohuatao@huawei.com>

--- CI/test_cases/container_cases/start.sh
+++ CI/test_cases/container_cases/start.sh
@@ -44,11 +44,77 @@ function do_test_t()
     return $TC_RET_T
 }
 
-ret=0
+function do_attach_local_test_t()
+{
+    local ret=0
+    local image="busybox"
+    local test="container start with --attach local test => (${FUNCNAME[@]})"
 
-do_test_t
-if [ $? -ne 0 ];then
-    let "ret=$ret + 1"
-fi
+    containername=start_attach
+    isula create -ti --name $containername busybox /bin/sh -c 'echo "hello"'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to create ${containername}" && ((ret++))
+    testcontainer $containername inited
 
-show_result $ret "basic start"
+    result=`isula start -a $containername`
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to start -a ${containername}" && ((ret++))
+    testcontainer $containername exited
+
+    isula rm -f $containername
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to rm container" && ((ret++))
+
+    id=`isula create -ti busybox /bin/sh -c 'ech "hello"'`
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to create container" && ((ret++))
+
+    isula start -a $id
+    [[ $? -eq 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - start container success, not as expected:failed" && ((ret++))
+
+    isula rm -f $id
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - isula rm container" && ((ret++))
+
+    msg_info "${test} finished with return ${ret}..."
+    return ${ret}
+}
+
+function do_attach_remote_test_t()
+{
+    local ret=0
+    local image="busybox"
+    local config='tcp://127.0.0.1:2890'
+    local test="container start with --attach remote test => (${FUNCNAME[@]})"
+
+    check_valgrind_log
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - stop isulad failed" && ((ret++))
+
+    start_isulad_with_valgrind -H "$config"
+
+    containername=start_attach
+    isula create -ti -H "$config" --name $containername busybox /bin/sh -c 'echo "hello"'
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to create ${containername} remote" && ((ret++))
+    testcontainer $containername inited
+
+    result=`isula start -a -H "$config" $containername`
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to start -a ${containername} remote" && ((ret++))
+    testcontainer $containername exited
+
+    isula rm -f -H "$config" $containername
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to rm container remote" && ((ret++))
+
+    check_valgrind_log
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - stop isulad failed" && ((ret++))
+
+    start_isulad_with_valgrind
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - start isulad failed" && ((ret++))
+
+    msg_info "${test} finished with return ${ret}..."
+    return ${ret}
+}
+
+declare -i ans=0
+
+do_test_t || ((ans++))
+
+do_attach_local_test_t || ((ans++))
+
+do_attach_remote_test_t || ((ans++))
+
+show_result ${ans} "${curr_path}/${0}"
