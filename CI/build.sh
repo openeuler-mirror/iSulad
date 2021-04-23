d6cc390f40a2c3eb0c37a1ea13634c4c33c81362
Li Feng <lifeng2221dd1@zoho.com.cn>
2021-04-23 09:40:13 +0800

    CI: keep container when build failed for debug
    Signed-off-by: Li Feng <lifeng2221dd1@zoho.com.cn>

--- CI/build.sh
+++ CI/build.sh
@@ -473,12 +473,12 @@ if [[ -e $CIDIR/${CONTAINER_NAME}.runflag ]]; then
     rm -rf /var/lib/isulad/${CONTAINER_NAME}_cptemp
     exit 0;
 else
-    for container in ${containers[@]}
-    do
-        docker rm -f $container
-        rm -rf /var/lib/isulad/$container
-    done
-    rm -rf /var/lib/isulad/${CONTAINER_NAME}_cptemp
+    #for container in ${containers[@]}
+    #do
+    #    docker rm -f $container
+    #    rm -rf /var/lib/isulad/$container
+    #done
+    #rm -rf /var/lib/isulad/${CONTAINER_NAME}_cptemp
     echo_error "Test failed!"
     exit -1;
 fi
