d8a0d9e4cb39d5a725f0fe38d360bcfed5de88a4
WangFengTu <wangfengtu@huawei.com>
2022-03-23 11:57:47 +0800

    remove temporary file only when write file failed
    Signed-off-by: WangFengTu <wangfengtu@huawei.com>

--- src/utils/cutils/utils_file.c
+++ src/utils/cutils/utils_file.c
@@ -1507,9 +1507,6 @@ static int do_atomic_write_file(const char *fname, const char *content, size_t c
 free_out:
     if (dst_fd >= 0) {
         close(dst_fd);
-        if (unlink(fname) != 0 && errno != ENOENT) {
-            SYSERROR("Failed to remove temp file:%s", fname);
-        }
     }
     return ret;
 }
@@ -1554,6 +1551,9 @@ int util_atomic_write_file(const char *fname, const char *content, size_t conten
 
 free_out:
     free(tmp_file);
+    if (ret != 0 && unlink(tmp_file) != 0 && errno != ENOENT) {
+        SYSERROR("Failed to remove temp file:%s", tmp_file);
+    }
     return ret;
 }
 
