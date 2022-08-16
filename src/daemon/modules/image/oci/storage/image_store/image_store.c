a8186dcb37cab9e96b1ccdda55a926988146877c
haozi007 <liuhao27@huawei.com>
2022-08-16 16:12:13 +0800

    [clang-anaylzer] ensure derenference of non-null pointer
    1. ensure derenference non-null pointer;
    2. fix double free;
    
    Signed-off-by: haozi007 <liuhao27@huawei.com>

--- src/daemon/modules/image/oci/storage/image_store/image_store.c
+++ src/daemon/modules/image/oci/storage/image_store/image_store.c
@@ -2025,7 +2025,7 @@ static bool validate_digest(const char *digest)
     char *encode = NULL;
 
     // contains ':' and is not the last character
-    if (index == NULL && index - value + 1 == strlen(value)) {
+    if (index == NULL || index - value + 1 == strlen(value)) {
         INFO("Invalid checksum digest format");
         ret = false;
         goto out;
