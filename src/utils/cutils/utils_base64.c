3ff2059b5fe04a7fdcf072f66fbd18dc21cdd484
WangFengTu <wangfengtu@huawei.com>
2022-08-18 10:21:50 +0800

    revert: add ENABLE_BORINGSSL to support android with openssl
    Signed-off-by: WangFengTu <wangfengtu@huawei.com>

--- src/utils/cutils/utils_base64.c
+++ src/utils/cutils/utils_base64.c
@@ -24,7 +24,7 @@
 #include "openssl/bio.h"
 #include "utils.h"
 
-#ifdef ENABLE_BORINGSSL
+#ifdef __ANDROID__
 int util_base64_encode(unsigned char *bytes, size_t len, char **out)
 {
     uint8_t *encoded = NULL;
