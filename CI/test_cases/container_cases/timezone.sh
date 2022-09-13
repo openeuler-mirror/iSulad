9bceb25e335238f4117af6e49f5fc93a88ec107d
root <root@LAPTOP-6O44CJ3O.localdomain>
2022-09-13 09:24:13 +0000

    Modify timezone CI test
    Signed-off-by: zhongtao <taozh97@163.com>

--- CI/test_cases/container_cases/timezone.sh
+++ CI/test_cases/container_cases/timezone.sh
@@ -27,8 +27,11 @@ function do_check_timezone()
     ln -sf $1 /etc/localtime
     localtime=`date "+%:z" | sed 's/://g'`
 
-    stop_isulad_with_valgrind
+    check_valgrind_log
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - stop isulad failed" && return ${FAILURE}
+
     start_isulad_with_valgrind
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - start isulad failed" && return ${FAILURE}
 
     containername="test_timezone"
 
@@ -65,8 +68,11 @@ function test_timezone()
     isula rmi ${image}
     [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to remove image ${image}" && return ${FAILURE}
 
-    stop_isulad_with_valgrind
+    check_valgrind_log
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - stop isulad failed" && return ${FAILURE}
+
     start_isulad_with_valgrind
+    [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - start isulad failed" && return ${FAILURE}
 
     msg_info "${test} finished with return ${TC_RET_T}..."
     return $TC_RET_T
