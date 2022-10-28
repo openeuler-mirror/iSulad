b0022ff6d820d17428aa04e6be148e84dade0855
haozi007 <liuhao27@huawei.com>
2022-10-28 16:32:03 +0800

    [improve] debug information for console io
    Signed-off-by: haozi007 <liuhao27@huawei.com>

--- src/utils/cutils/io_wrapper.h
+++ src/utils/cutils/io_wrapper.h
@@ -21,6 +21,15 @@
 extern "C" {
 #endif
 
+enum {
+    FIFO_IN_IO = 0,
+    FIFO_OUT_IO,
+    FIFO_ERR_IO,
+    FUNC_IN_IO,
+    FUNC_OUT_IO,
+    FUNC_ERR_IO,
+};
+
 typedef ssize_t (*io_write_func_t)(void *context, const void *data, size_t len);
 typedef int (*io_close_func_t)(void *context, char **err);
 
@@ -28,6 +37,7 @@ struct io_write_wrapper {
     void *context;
     io_write_func_t write_func;
     io_close_func_t close_func;
+    int io_type;
 };
 
 typedef ssize_t (*io_read_func_t)(void *context, void *buf, size_t len);
@@ -36,6 +46,7 @@ struct io_read_wrapper {
     void *context;
     io_read_func_t read;
     io_close_func_t close;
+    int io_type;
 };
 
 #ifdef __cplusplus
