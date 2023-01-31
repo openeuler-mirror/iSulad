69e325f57f401f913c8913502233b534ecb04b36
ilya.kuksenok <ilya.kuksenok@huawei.com>
2023-01-31 13:23:19 +0300

    add api changes fix build flags add gogo.proto and fix old gogo.proto related bug add cmake protoc for gogo.proto remove unnecessary C_FLAGS

--- cmake/set_build_flags.cmake
+++ cmake/set_build_flags.cmake
@@ -3,7 +3,7 @@ set(CMAKE_C_FLAGS "-fPIC -fstack-protector-all -D_FORTIFY_SOURCE=2 -O2 -Wall -fP
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D__FILENAME__='\"$(subst ${CMAKE_SOURCE_DIR}/,,$(abspath $<))\"'")
 
 if (GRPC_CONNECTOR)
-    set(CMAKE_CXX_FLAGS "-fPIC -std=c++11 -fstack-protector-all -D_FORTIFY_SOURCE=2 -O2 -Wall")
+    set(CMAKE_CXX_FLAGS "-fPIC -std=c++11 -fstack-protector-all -D_FORTIFY_SOURCE=2 -O2 -Wall -Wno-error=deprecated-declarations")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__FILENAME__='\"$(subst ${CMAKE_SOURCE_DIR}/,,$(abspath $<))\"'")
 endif()
 set(CMAKE_SHARED_LINKER_FLAGS "-Wl,-E -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -Wtrampolines -shared -pthread")
@@ -16,7 +16,7 @@ endif()
 
 if (ISULAD_GCOV)
     set(CMAKE_C_FLAGS_DEBUG "-Wall -fprofile-arcs -ftest-coverage")
-    set(CMAKE_CXX_FLAGS_DEBUG "-Wall -fprofile-arcs -ftest-coverage")
+    set(CMAKE_CXX_FLAGS_DEBUG "-Wall -fprofile-arcs -ftest-coverage -Wno-error=deprecated-declarations")
     message("-----CXXFLAGS: " ${CMAKE_CXX_FLAGS_DEBUG})
     message("------compile with gcov-------------")
     message("-----CFLAGS: " ${CMAKE_C_FLAGS_DEBUG})
