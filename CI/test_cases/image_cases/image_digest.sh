ef4aa65c20b37a45d385ff5f14d9f127bf4a71a6
zhongtao <zhongtao17@huawei.com>
2023-06-05 02:11:34 +0000

    !2040 fix image_digest.sh * fix image_digest.sh

--- CI/test_cases/image_cases/image_digest.sh
+++ CI/test_cases/image_cases/image_digest.sh
@@ -31,22 +31,22 @@ function test_image_with_digest()
 
   msg_info "${test} starting..."
 
-  isula pull ${image_digest}
+  isula pull docker.io/library/${image_digest}
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to pull image: ${image}" && return ${FAILURE}
   
-  isula tag ${image_digest} ${image}:latest
+  isula tag ${image_digest} ${image}:digest_test
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to tag image: ${image}" && return ${FAILURE}
 
   isula images | grep busybox
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - missing list image: ${image}" && ((ret++))
 
-  isula inspect -f '{{.image.id}}' ${image} | grep -E '^[0-9a-f]{64}$'
+  isula inspect -f '{{.image.id}}' ${image}:digest_test | grep -E '^[0-9a-f]{64}$'
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - invalid image id: ${image}" && ((ret++))
 
-  isula inspect -f '{{.image.repo_digests}}' ${image} | grep -E "[\s\D]*${image}@sha256:[0-9a-f]{64}[\s\D]*"
+  isula inspect -f '{{.image.repo_digests}}' ${image}:digest_test | grep -E "[\s\D]*${image}@sha256:[0-9a-f]{64}[\s\D]*"
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - invalid image repo digests: ${image}" && ((ret++))
 
-  isula inspect -f '{{.image.repo_tags}}' ${image_digest} | grep "${image}:latest"
+  isula inspect -f '{{.image.repo_tags}}' ${image_digest} | grep "${image}:digest_test"
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - invalid image digest: ${image_digest}" && ((ret++))
 
   isula run -tid --name test ${image_digest} sh
@@ -55,7 +55,7 @@ function test_image_with_digest()
   isula rm -f test
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to rm container" && ((ret++))
   
-  isula run -tid --name test ${image} sh
+  isula run -tid --name test ${image}:digest_test sh
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to run container with image tag: ${image}:latest" && ((ret++))
 
   isula rm -f test
@@ -64,8 +64,11 @@ function test_image_with_digest()
   isula inspect -f '{{.image.repo_tags}}' ${image} | grep "${image}:latest"
   [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - invalid image repo tags: ${image}" && ((ret++))
 
-  isula rmi ${image}
-  [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to remove image ${image}" && ((ret++))
+  isula rmi ${image_digest}
+  [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to remove image ${image_digest}" && ((ret++))
+
+  isula rmi ${image}:digest_test
+  [[ $? -ne 0 ]] && msg_err "${FUNCNAME[0]}:${LINENO} - failed to remove image ${image}:digest_test" && ((ret++))
 
   msg_info "${test} finished with return ${ret}..."
   return ${ret}
