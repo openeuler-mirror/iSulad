bcd119b46e104750c6ee33d6488bb1bb356d2cc2
haozi007 <liuhao27@huawei.com>
2023-06-07 06:59:20 +0000

    !2055 adapt lcr code refactor * adapt lcr code refactor

--- CI/install_depends.sh
+++ CI/install_depends.sh
@@ -123,7 +123,7 @@ ldconfig
 cd ~
 git clone https://gitee.com/openeuler/lcr.git
 cd lcr
-sed -i 's/fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO/fd == 0 || fd == 1 || fd == 2 || fd >= 1000/g' ./src/utils.c
+sed -i 's/fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO/fd == 0 || fd == 1 || fd == 2 || fd >= 1000/g' ./src/utils/utils_file.c
 mkdir -p build
 cd build
 cmake -DENABLE_UT=ON -DLIB_INSTALL_DIR=${builddir}/lib -DCMAKE_INSTALL_PREFIX=${builddir} ../
